16f5060076601ac2177a0f1dedd601a870accff2
김영우 <kyw05171@gmail.com>
2021-05-25 07:26:38 +0900

    "Charter 08. 스타일 가이드" 를 새로 작성 하였습니다.

--- 8-style-guide-storybook/src/router/index.ts
+++ 8-style-guide-storybook/src/router/index.ts
@@ -1,25 +1,30 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
+import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-	{
-		path: "/",
-		component: () => import("@/views/Home.vue"),
-	},
-
-	// Button.vue
-	{
-		path: "/pt-button",
-		component: () => import("@/views/PT_Button.vue"),
-	},
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
 ];
 
 const router = new VueRouter({
-	mode: "history",
-	base: process.env.BASE_URL,
-	routes,
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
 });
 
 export default router;
--- 8-style-guide-storybook/src/views/Home.vue
+++ 8-style-guide-storybook/src/views/Home.vue
@@ -1,25 +1,18 @@
 <template>
-	<div id="home">
-		<img alt="Vue logo" src="../assets/logo.png" />
-		<h1 class="global_h1">Vue를 사용한 CSS 설계 정리</h1>
-		<h3 class="global_h3">with <span class="accent"> Storybook </span></h3>
-	</div>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-@Component
-export default class Home extends Vue {
-	//
-}
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
 </script>
-
-<style lang="scss">
-#home {
-	& .accent {
-		color: #ff1493;
-		font-size: 1.5rem;
-	}
-}
-</style>
